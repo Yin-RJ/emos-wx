75c5661add262018ebd76cf4554982802c3edd10
YinRJ <yin_rj@qq.com>
2022-03-28 23:40:28 +0800

    demo页面

---
+++ pages/demo/demo.vue
@@ -0,0 +1,41 @@
+<template>
+	<view>
+		<view>{{user_name}}</view>
+		<view v-for="one in tel">{{one}}</view>
+		
+		<view v-if="age >= 18">
+			<button @tap="signUp()">我要报名</button>
+		</view>
+		
+		<view>
+			<input type="text" v-model="address" placeholder="输入地址"/>
+		</view>
+		<view>{{address}}</view>
+	</view>
+
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				user_name: "Scott",
+				tel: ["13312345678", "15912345678"],
+				age: 14,
+				address: ""
+			};
+		},
+		methods:{
+			signUp: function() {
+				uni.showToast({
+					title:"点击了按钮"
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
+
