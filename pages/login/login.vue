dd9ca8bb724133d512305127dff54923dc6d9d48
YinRJ <yin_rj@qq.com>
2022-03-28 23:59:52 +0800

    增加登录页面

---
+++ pages/login/login.vue
@@ -0,0 +1,34 @@
+<template>
+	<view>
+		<image src="../../static/logo-1.png" class="logo" mode="widthFix"></image>
+		<view class="logo-title">EMOS企业办公</view>
+		<view class="logo-subtitle">Ver 2050.2</view>
+		<button class="login-btn" open-type="getUserInfo" @tap="login()">登录系统</button>
+		<view class="register-container">
+			没有账号？
+			<text class="register" @tap="toRegister()">立即注册</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			login() {
+				
+			},
+			toRegister() {
+				
+			}
+		}
+	}
+</script>
+
+<style lang="less">
+	@import url("login.less");
+</style>
